82f833ce7ef3727e595866af51ffb5d8fe86907a
Michael <michael.makram.zm@gmail.com>
2025-01-24 20:08:05 +0200

    Update MediaViewComponent.java

--- view/MediaViewComponent.java
+++ view/MediaViewComponent.java
@@ -1,7 +1,7 @@
 package com.example.videoplayer.ui.view;
 
 import com.example.videoplayer.adapter.presenter.MainPresenter;
- import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaView;
 
 /**
  * Component to handle Media View events
@@ -11,34 +11,34 @@ public class MediaViewComponent {
 
     /**
      * Constructor for media view class
-    * @param presenter Application presenter
+     * @param presenter Application presenter
      */
-   public MediaViewComponent(MainPresenter presenter) {
-         this.mediaView = new MediaView();
-          presenter.observePlayerState()
-                .subscribe(playerState -> {
-                    if(playerState.status() != null) {
-                       if (playerState.status().ordinal() == 2) {
-                         presenter.stop().blockingGet();
-                      }
-                    }
-                   mediaView.setMediaPlayer(presenter.getMediaPlayer());
-               },
-                    error -> {
-                        // Do nothing for now.
-                     }
-                 );
+    public MediaViewComponent(MainPresenter presenter) {
+        this.mediaView = new MediaView();
+        presenter.observePlayerState()
+                  .subscribe(playerState -> {
+                              if(playerState.status() != null) {
+                                 if (playerState.status().ordinal() == 2) {
+                                     presenter.stop().blockingGet();
+                                 }
+                               }
+                              mediaView.setMediaPlayer(presenter.getMediaPlayer());
+                          },
+                        error -> {
+                            // Do nothing for now.
+                         }
+                       );
         this.mediaView.setFitWidth(800);
         this.mediaView.setFitHeight(600);
-         this.mediaView.setPreserveRatio(true);
-        this.mediaView.setSmooth(true);
-   }
+        this.mediaView.setPreserveRatio(true);
+       this.mediaView.setSmooth(true);
+    }
 
     /**
-      * Returns the MediaView instance
+     * Returns the MediaView instance
      * @return MediaView instance
      */
-   public MediaView getMediaView() {
-         return mediaView;
+    public MediaView getMediaView() {
+        return mediaView;
     }
 }
