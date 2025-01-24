f1aedc1973eb0c894e15695e8e826b2cecdae9d5
Michael <michael.makram.zm@gmail.com>
2025-01-24 20:02:02 +0200

    Update MediaLoadException.java

--- exception/MediaLoadException.java
+++ exception/MediaLoadException.java
@@ -1,10 +1,22 @@
 package com.example.videoplayer.core.exception;
 
+/**
+* Custom exception for media loading errors.
+ */
 public class MediaLoadException extends RuntimeException {
+    /**
+     * Constructor for MediaLoadException.
+     * @param message Error message.
+     */
     public MediaLoadException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor for MediaLoadException.
+     * @param message Error message.
+     * @param cause Cause of exception.
+     */
     public MediaLoadException(String message, Throwable cause) {
         super(message, cause);
     }
