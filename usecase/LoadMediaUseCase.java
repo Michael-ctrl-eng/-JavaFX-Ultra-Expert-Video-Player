a5382596681457e9b4fcd95d34a7e296a5a4d0de
Michael <michael.makram.zm@gmail.com>
2025-01-24 20:01:00 +0200

    Update LoadMediaUseCase.java

--- usecase/LoadMediaUseCase.java
+++ usecase/LoadMediaUseCase.java
@@ -11,7 +11,24 @@ import java.util.List;
  * Interface for loading media, subtitles, and metadata.
  */
 public interface LoadMediaUseCase {
+    /**
+     * Loads the media from the given file.
+     * @param file The file to load.
+     * @return A Single emitting the PlayerState.
+     */
     Single<PlayerState> loadMedia(File file);
+
+    /**
+     * Loads the subtitles from the given file.
+     * @param file The file to load the subtitles from.
+     * @return A Single emitting a List of Subtitles.
+     */
     Single<List<Subtitle>> loadSubtitle(File file);
+
+    /**
+     * Loads the metadata of the media from the given file.
+     * @param file The file to load the metadata from.
+     * @return A Single emitting the MediaMetadata.
+     */
     Single<MediaMetadata> loadMetadata(File file);
 }
