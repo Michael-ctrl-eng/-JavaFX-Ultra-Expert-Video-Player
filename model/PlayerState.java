119c56d35339666a56ffbccd2e93e5e8819fdfd6
Michael <michael.makram.zm@gmail.com>
2025-01-24 20:00:25 +0200

    Update PlayerState.java

--- model/PlayerState.java
+++ model/PlayerState.java
@@ -4,6 +4,12 @@ import javafx.scene.media.MediaPlayer;
 
 /**
  * Represents the state of the media player. Immutable.
+ * @param status The status of the media player.
+ * @param currentTime The current time of the media player.
+ * @param volume The volume of the media player.
+ * @param rate The playback rate of the media player.
+ * @param subtitleText The text of the current subtitle.
  */
 public record PlayerState(MediaPlayer.Status status, double currentTime, double volume, double rate, String subtitleText) {
-}
+    // Immutability by record
+ }
