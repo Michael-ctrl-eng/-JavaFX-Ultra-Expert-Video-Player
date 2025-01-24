be9c1cbe8b68cc795f362276727e7f09c5d4bf26
Michael <michael.makram.zm@gmail.com>
2025-01-24 20:08:54 +0200

    Update MediaControls.java

--- view/MediaControls.java
+++ view/MediaControls.java
@@ -62,9 +62,9 @@ public class MediaControls {
                     .take(1)
                     .subscribe(playerState -> {
                         if (playerState.status() != null && playerState.status().ordinal() == 2) {
-                            presenter.pause()
-                                    .subscribe(playerState1 ->  playButton.setText("Play"),
-                                                error -> UIHelper.showAlert("Error","Error occurred", "Error pausing video"));
+                             presenter.pause()
+                                        .subscribe(playerState1 ->  playButton.setText("Play"),
+                                                    error -> UIHelper.showAlert("Error","Error occurred", "Error pausing video"));
                         } else {
                             presenter.play()
                                       .subscribe(playerState1 ->  playButton.setText("Pause"),
@@ -76,79 +76,80 @@ public class MediaControls {
         );
         //seek slider logic
         disposables.add(presenter.observePlayerState()
-                 .subscribe(playerState -> {
-                        if(!seekSlider.isValueChanging())
-                            seekSlider.setValue(playerState.currentTime() / presenter.observePlayerState().blockingFirst().currentTime() * 100);
-                    },
-                     error -> {
-                          UIHelper.showAlert("Error","Error occurred", "Error setting up time slider");
-                     }
-               )
+                .subscribe(playerState -> {
+                            if(!seekSlider.isValueChanging())
+                                seekSlider.setValue(playerState.currentTime() / presenter.observePlayerState().blockingFirst().currentTime() * 100);
+                        },
+                        error -> {
+                            UIHelper.showAlert("Error","Error occurred", "Error setting up time slider");
+                        }
+                )
         );
-       seekSlider.valueProperty().addListener(observable -> {
-            if(seekSlider.isValueChanging()){
+        seekSlider.valueProperty().addListener(observable -> {
+             if(seekSlider.isValueChanging()){
                 double value = seekSlider.getValue()/100;
                 presenter.seek(value)
-                        .subscribe(playerState -> {
+                         .subscribe(playerState -> {
 
-                                 },
+                                },
                             error -> {
                                  UIHelper.showAlert("Error", "Error", "Error seeking video");
                              }
-                            );
+                          );
             }
-       });
-         //volume slider logic
-         volumeSlider.valueProperty().addListener(observable -> presenter.setVolume(volumeSlider.getValue())
-                 .subscribe(playerState -> {
+         });
+        //volume slider logic
+        volumeSlider.valueProperty().addListener(observable -> presenter.setVolume(volumeSlider.getValue())
+                .subscribe(playerState -> {
 
-                           },
-                           error -> {
-                                UIHelper.showAlert("Error", "Error", "Error setting volume");
-                           }
-                  )
-         );
+                        },
+                        error -> {
+                            UIHelper.showAlert("Error", "Error", "Error setting volume");
+                       }
+                   )
+        );
+
         frameBackButton.setOnAction(e -> presenter.seekBackward()
-                  .subscribe(playerState -> {
-
-                            },
-                     error -> {
-                          UIHelper.showAlert("Error", "Error", "Error seeking backward");
-                     }
-                  )
-         );
-       frameForwardButton.setOnAction(e -> presenter.seekForward()
                  .subscribe(playerState -> {
 
+                             },
+                        error -> {
+                             UIHelper.showAlert("Error", "Error", "Error seeking backward");
+                         }
+                    )
+        );
+        frameForwardButton.setOnAction(e -> presenter.seekForward()
+                .subscribe(playerState -> {
+
                             },
-                     error -> {
-                          UIHelper.showAlert("Error", "Error", "Error seeking forward");
-                     }
-                 )
-       );
+                       error -> {
+                            UIHelper.showAlert("Error", "Error", "Error seeking forward");
+                        }
+                   )
+        );
         speedDownButton.setOnAction(e -> {
-            presenter.observePlayerState()
+           presenter.observePlayerState()
                     .take(1)
                     .subscribe(playerState -> {
                         double currentRate = playerState.rate();
                         double newRate = Math.max(Constants.MIN_PLAYBACK_RATE, currentRate - Constants.PLAYBACK_RATE_STEP);
-                       presenter.setRate(newRate)
-                                .subscribe(playerState1 -> updateSpeedLabel(newRate),
-                                         error -> UIHelper.showAlert("Error", "Error", "Error setting rate")
-                                        );
+                        presenter.setRate(newRate)
+                                 .subscribe(playerState1 -> updateSpeedLabel(newRate),
+                                          error -> UIHelper.showAlert("Error", "Error", "Error setting rate")
+                                         );
                     });
         });
         speedUpButton.setOnAction(e -> {
-            presenter.observePlayerState()
+           presenter.observePlayerState()
                     .take(1)
                     .subscribe(playerState -> {
-                        double currentRate = playerState.rate();
-                        double newRate = Math.min(Constants.MAX_PLAYBACK_RATE, currentRate + Constants.PLAYBACK_RATE_STEP);
+                       double currentRate = playerState.rate();
+                       double newRate = Math.min(Constants.MAX_PLAYBACK_RATE, currentRate + Constants.PLAYBACK_RATE_STEP);
                         presenter.setRate(newRate)
                                 .subscribe(playerState1 -> updateSpeedLabel(newRate),
-                                        error -> UIHelper.showAlert("Error", "Error", "Error setting rate")
-                                );
-                   });
+                                          error -> UIHelper.showAlert("Error", "Error", "Error setting rate")
+                                    );
+                    });
         });
     }
 
