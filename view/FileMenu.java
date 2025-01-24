d41bf186a6ef2b582f1dfcace16b42bab13015ce
Michael <michael.makram.zm@gmail.com>
2025-01-24 16:13:42 +0200

    Update FileMenu.java

--- view/FileMenu.java
+++ view/FileMenu.java
@@ -1,6 +1,7 @@
 package com.example.videoplayer.ui.view;
 
 import com.example.videoplayer.adapter.presenter.MainPresenter;
+import com.example.videoplayer.ui.utils.UIHelper;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -28,8 +29,14 @@ public class FileMenu {
             fileChooser.setTitle("Open Video File");
             File file = fileChooser.showOpenDialog(null);
             if(Objects.nonNull(file)){
-                presenter.loadMedia(file);
-            }
+                presenter.loadMedia(file)
+                        .subscribe(playerState -> {
+
+                                },
+                                error -> {
+                                    UIHelper.showAlert("Error","Error occurred", "Error loading media");
+                                  });
+             }
         });
         MenuItem openSubtitle = new MenuItem("Open Subtitle");
         openSubtitle.setOnAction(e -> {
@@ -37,8 +44,15 @@ public class FileMenu {
             fileChooser.setTitle("Open Subtitle File");
             File file = fileChooser.showOpenDialog(null);
             if(Objects.nonNull(file)){
-                presenter.loadSubtitle(file);
-            }
+               presenter.loadSubtitle(file)
+                        .subscribe(subtitles -> {
+
+                                },
+                                error -> {
+                                    UIHelper.showAlert("Error","Error occurred", "Error loading subtitle");
+                                }
+                        );
+             }
         });
         fileMenu.getItems().addAll(openFile, openSubtitle);
         menuBar.getMenus().add(fileMenu);
